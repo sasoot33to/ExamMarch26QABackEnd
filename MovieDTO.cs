ff231babc6b44ab4d63ea06b2ba01265a9313cd0
sasoot33to <sasoot33to@gmail.com>
2026-04-18 13:27:14 +0300

    Add files via upload

---
+++ MovieDTO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks
+
+namespace MovieCatalog.Models;
+
+public class MoviDto
+{
+	[JsonPropertyName("id")]
+
+	public string Id { get; set; } = string.Empty;
+
+	[JsonPropertyName("title")]
+
+	public string Title { get; set; } = string.Empty;
+
+	[JsonPropertyName("description")]
+
+	public string Description { get; set; } = string.Empty;
+}
---
+++ MoviesExam/DTO/ApiRespomseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MoviesExam.DTO;
+
+public class ApiResponseDto
+{
+    [JsonPropertyName("msg")]
+
+    public string Msg { get; set; }
+
+    [JsonPropertyName("movie")]
+
+    public MovieDto Movie { get; set; } = new MovieDto();
+}
